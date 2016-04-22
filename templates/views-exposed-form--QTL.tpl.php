b672dcc6e3800a76f55590bf040c338fdd1f8ee0
E.Cannon <ekcannon@iastsate.edu>
2016-04-22 11:36:22 -0500

    initial import

---
+++ templates/views-exposed-form--QTL.tpl.php
@@ -0,0 +1,20 @@
+<?php
+  $sql = "SELECT COUNT(*) FROM {qtl_search}";
+  if ($res = chado_query($sql, array())) {
+    $row = $res->fetchObject();
+    $num_qtl = $row->count;
+  }
+?>
+<h3>Search Arachis QTL</h3>
+<p>
+  All fields are optional and partial names are accepted. 
+  Click column headers to sort.
+</p>
+
+<?php 
+  if ($num_qtl) {
+    echo "Total QTL count: <b>$num_qtl</b><br>";
+  }
+  
+  include_once("sites/all/modules/legume/legume_qtl/theme/templates/views-exposed-form--QTL.tpl.php"); 
+?>
