d8358869015d5f375e740a7e6bc59ee7411910a6
E.Cannon <ekcannon@iastsate.edu>
2016-05-26 11:26:12 -0500

    Tweaked text

--- templates/views-exposed-form--QTL.tpl.php
+++ templates/views-exposed-form--QTL.tpl.php
@@ -13,7 +13,7 @@
 
 <?php 
   if ($num_qtl) {
-    echo "Total QTL count: <b>$num_qtl</b><br>";
+    echo "Total QTL in PeanutBase: <b>$num_qtl</b><br>";
   }
   
   include_once("sites/all/modules/legume/legume_qtl/theme/templates/views-exposed-form--QTL.tpl.php"); 
