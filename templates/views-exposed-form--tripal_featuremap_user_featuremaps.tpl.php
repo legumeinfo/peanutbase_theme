075000fce16b08dd066df43f159a77b990f52f50
E.Cannon <ekcannon@iastsate.edu>
2018-08-30 11:30:41 -0500

    Tweaked instructions a wee bit and added explanation of map names.

--- templates/views-exposed-form--tripal_featuremap_user_featuremaps.tpl.php
+++ templates/views-exposed-form--tripal_featuremap_user_featuremaps.tpl.php
@@ -18,7 +18,11 @@
  *
  * @ingroup views_templates
  */
+ 
+  // collapsable div support
+  drupal_add_library('system', 'drupal.collapse');
 ?>
+<script type="text/javascript" src="https://peanutbase-dev.usda.iastate.edu/misc/collapse.js?v=7.59"></script>
 <?php if (!empty($q)): ?>
   <?php
     // This ensures that, if clean URLs are off, the 'q' is added first so that
@@ -28,10 +32,27 @@
 <?php endif; ?>
 <h2>Map Search</h2>
 Please enter text for the name and/or description for a map and click "Search". 
-Leave both fields empty and click search to see a list of all maps.
-<br>
 For example, search for "SunOleic" or "duranensis".
-
+<fieldset class=" collapsible collapsed">
+  <legend>
+    <span class="fieldset-legend"><a href="#" class="fieldset-title">About the map names...</a></span> 
+  </legend>
+  <div  class="fieldset-wrapper">
+    <p> 
+      Most maps are renamed differently from their published names. This is to make them
+      more easily identifiable by ploidy, genome, and parents. 
+    </p>
+    <p>The Map names are 
+      constructed from: [ploidy/genome]_[parent1]_x_[parent2]_[a..z], for example, 
+      TT_Tifrunner_x_GT-C20_a has ploidy/genome T (tetraploid) in both parents, 
+      Tifrunner is parent 1, and GT-C20 is parent 2. The a indicates that this is the 
+      first map loaded derived from the parents used to produce this mapping population.
+      Another example is AA_A.duranensis_x_A.duranensis_a. This indicates that the map
+      includes the A genome, and both parents are from the species <i>A. duranensis</i>.
+    </p>
+  </div>
+</fieldset>
+Leave both fields empty and click search to see a list of all maps.
 <div class="views-exposed-form">
   <div class="views-exposed-widgets clearfix">
     <?php foreach ($widgets as $id => $widget): ?>
